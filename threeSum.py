90b3340594900db9ae8412b547b13c8cbe6158f3
Kashav Piya <71453613+kashavpiya@users.noreply.github.com>
2024-09-17 13:07:43 -0500

    added threeSum

---
+++ threeSum.py
@@ -0,0 +1,31 @@
+
+def threeSum(nums):
+    nums.sort()
+    res = []
+
+    for i, n in enumerate(nums):
+        if i > 0 and n == nums[i - 1]:
+            continue
+
+        l, r = i + 1, len(nums) - 1
+
+        while l < r:
+            threeSum = n + nums[l] + nums[r]
+            if threeSum > 0:
+                r -= 1
+            elif threeSum < 0:
+                l += 1
+            else:
+                res.append([n, nums[l], nums[r]])
+                l += 1
+                while nums[l] == nums[l - 1] and l < r:
+                    l += 1
+
+    return res
+
+
+def main():
+    print(threeSum([-1,0,1,2,-1,-4]))
+
+if __name__ == "__main__":
+    main()
