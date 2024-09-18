2be812fafe62bd5bc48675730aacc2430cde2f55
Kashav Piya <71453613+kashavpiya@users.noreply.github.com>
2024-09-18 09:44:07 -0500

    added Cycle Linked List

---
+++ cyclList.py
@@ -0,0 +1,9 @@
+def detectCycle(head):
+    slow, fast = head, head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            return True
+    return False
