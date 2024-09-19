2f313db49dfba3c790dc1a1cf68674dbc6ee2b24
Kashav Piya <71453613+kashavpiya@users.noreply.github.com>
2024-09-19 12:30:05 -0500

    Create peakElement.py

---
+++ peakElement.py
@@ -0,0 +1,12 @@
+def findPeakElement(self, nums: List[int]) -> int:
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        m = l + ((r - l) // 2)
+
+        if m > 0 and nums[m] < nums[m - 1]:
+            r = m - 1
+        elif m < r and nums[m] < nums[m + 1]:
+            l = m + 1
+        else:
+            return m
